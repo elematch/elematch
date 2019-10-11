e33d339eea48cce15552818ea14196a03ab66d4a
Thomas Schäfer <t.schaefer@headtrip.eu>
2019-10-12 01:07:31 +0200

    Add static functions to check if a set of 3 cards match

--- src/util/entity/Card.js
+++ src/util/entity/Card.js
@@ -6,7 +6,37 @@ export class Card {
     this.level = level
   }
 
-  static doMatch ([card1, card2, card3]) {
+  /**
+   *
+   * @param cards Card[]
+   * @return {boolean | string}
+   */
+  static doCardsMatch (cards) {
+    const elementsMatch = Card.doesPropertyMatch(cards, 'element')
+    const countsMatch = Card.doesPropertyMatch(cards, 'count')
+    const colorsMatch = Card.doesPropertyMatch(cards, 'color')
+    const levelsMatch = Card.doesPropertyMatch(cards, 'level')
+
+    return (elementsMatch && countsMatch && colorsMatch && levelsMatch)
+  }
+
+  /**
+   *
+   * @param cards Card[]
+   * @param propertyName string
+   */
+  static doesPropertyMatch (cards, propertyName) {
+    const [card1, card2, card3] = cards
+    if (card1[propertyName] === card2[propertyName] && card2[propertyName] === card3[propertyName]) {
+      return true
+    }
+    if (
+      card1[propertyName] !== card2[propertyName]
+      && card2[propertyName] !== card3[propertyName]
+      && card3[propertyName] !== card1[propertyName]
+    ) {
+      return true
+    }
     return false
   }
 }
