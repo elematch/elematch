772688e44184b779ae4617b6fc2a9910e81fd76a
j-brn <me@jbrn.eu>
2019-10-12 15:15:10 +0200

    Add function to get the texture name for a card

--- src/util/entity/Card.js
+++ src/util/entity/Card.js
@@ -1,3 +1,19 @@
+const ELEMENT = {
+  1: "fire",
+  2: "watter",
+  3: "lightning"
+};
+
+const COLOR = {
+  1: "red",
+  2: "blue",
+  3: "green"
+};
+
+export let getTextureNameForCard = (card) => {
+  return ELEMENT[card.element] + "-" + COLOR[card.color] + "-" + card.count;
+};
+
 export class Card {
   constructor({element, count, color, level}) {
     this.element = element;
