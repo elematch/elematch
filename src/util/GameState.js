03135b3722d685ac57d8c370c68a70e51c66e857
j-brn <me@jbrn.eu>
2019-10-12 15:51:35 +0200

    replace set/unset card function with a toggle

--- src/util/GameState.js
+++ src/util/GameState.js
@@ -34,29 +34,27 @@ export class GameState {
         return this.time > 0;
     }
 
-    selectCard(card) {
-        this.clickedCards.add(card);
+    toggleCard(card) {
+        if (this.clickedCards.has(card)) {
+            this.clickedCards.delete(card);
+        } else {
+            this.clickedCards.add(card);
 
-        if (set.length === 3) {
-            if (isValidSet(...this.clickedCards.map(card => { return card.data}))) {
-                this.score += POINTS_PER_SET;
-                this.clickedCards.clear();
-                this.newDeck = true;
-            } else {
-                this.time -= TIME_LOSS_PER_FAILURE;
-                this.lives -= 1;
+            if (set.length === 3) {
+                if (isValidSet(...this.clickedCards.map(card => { return card.data}))) {
+                    this.score += POINTS_PER_SET;
+                    this.clickedCards.clear();
+                    this.newDeck = true;
+                } else {
+                    this.time -= TIME_LOSS_PER_FAILURE;
+                    this.lives -= 1;
 
-                if (this.lives === 0) {
-                    this.lives = LIVES;
-                    this.score -= POINT_LOSS_ON_MISSING_LIVES;
+                    if (this.lives === 0) {
+                        this.lives = LIVES;
+                        this.score -= POINT_LOSS_ON_MISSING_LIVES;
+                    }
                 }
             }
-        }
-    }
-
-    unselectCard(card) {
-        if (this.clickedCards.has(card)) {
-            this.clickedCards.delete(card)
         }
     }
 }
