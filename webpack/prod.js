35cdb0b3988c30c56f6ea1fc152ceb77cbac90e0
dehrenschwender <dehrenschwender@users.noreply.github.com>
2019-10-13 14:41:57 +0200

    add manifest with webpack
    add manifest with webpack

--- webpack/prod.js
+++ webpack/prod.js
@@ -3,6 +3,9 @@ const path = require("path");
 const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
 
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 module.exports = merge(base, {
   mode: "production",
   output: {
@@ -21,7 +24,16 @@ module.exports = merge(base, {
             comments: false
           }
         }
-      })
+      }),
+      new WebpackPwaManifest({
+        name: 'Elematch',
+        short_name: 'Elematch',
+        description: 'Elemettch!',
+        background_color: '#01579b',
+        theme_color: '#01579b',
+        'theme-color': '#01579b',
+        start_url: '/',
+      }),
     ]
   }
 });
